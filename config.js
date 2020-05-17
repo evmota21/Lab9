96336a174efa9ff9e9ff5182a1aed269f45ff29f
Enrique Mota <evmota@MacBook-Pro-de-Enrique.local>
2020-05-16 22:55:59 -0500

    Modified config.js file

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.DATABASE_URL = "mmongodb://localhost/bookmarksdb";
+exports.DATABASE_URL = "mongodb+srv://evmota21:rodrigo95@cluster0-div5y.mongodb.net/bookmarksdb?retryWrites=true&w=majority";
 exports.TOKEN = process.env.API_TOKEN || '2abbf7c3-245b-404f-9473-ade729ed4653';
 exports.PORT = process.env.PORT || '8080';
